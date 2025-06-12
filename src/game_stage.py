af0f01b1aeab3de3e84aee56014d836199a7dae1
Marcus Paiva <marcuspaiva2014@gmail.com>
2025-06-11 22:26:31 -0300

    Separating game engine and pause menu

--- src/game_stage.py
+++ src/game_stage.py
@@ -89,5 +89,47 @@ class Stage:
                 if keys[pygame.K_ESCAPE]:
                     self._pause = True
 
+    def __in_game(self):
+        """
+        In game.
+        """
+        tm = datetime.now() - self._prev_time
+        if tm > timedelta(milliseconds=100):
+            self._prev_time = datetime.now()
+            if not self._pause:
+                self._player.move()
+        self._player.update()
+        self._fruit.update()
+        self._fruit.draw()
+        self._player.draw()
+
+        if detect_player_fruit_collision(self._player, self._fruit):
+            self._fruit.generate()
+            self._player.add_point()
+
+        if self_collision(self._player):
+            self._collision += 1
+            self._pause = True
+            self._end_game = True
+            print(f"Colidiu! {self._collision}")
+
+    def __pause_menu(self):
+        if self._pause:
+            self._screen.blit(self._paused_text, (self._screen.get_width() / 2, self._screen.get_height() / 2))
+
+
+    def loop(self):
+        """
+        Main game loop.
+        """
+        while self._running:
+            self.__user_io_detection()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+            self._screen.fill("orange")
+            self.__in_game()
+            self.__pause_menu()
+
             pygame.display.flip()
             self._clock.tick(60)
