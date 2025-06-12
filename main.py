c07bcce4263d973a01cc40138094d60e8b14a985
Marcus Paiva <marcuspaiva2014@gmail.com>
2025-06-12 18:58:45 -0300

    Change screen size and add screen caption

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from src.SplashScreen import SplashScreen
 from src.game_stage import Stage
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1100, 720))
+pygame.display.set_caption("PySnake Game")
 
 splash = SplashScreen(screen)
 splash.show()
